7452b02a86dc4833fc041e45225ccb1c16a2089c
Filip Šikula <cz.filipsi@gmail.com>
2015-02-09 11:36:10 +0100

    Fixed CommunityUpdate page

--- questbook/communityupdate.php
+++ questbook/communityupdate.php
@@ -15,31 +15,33 @@
 		
 		<script>
 			$(document).ready(function(){
-				$('#TextWrapper').data("TopDefault" ,$('#TextWrapper').css('top'));
+				$('#TextWrapper').data("Scroll" ,"Down");
 
 				$("#DownArrow").click(function() {
 					if(!$('#TextWrapper').is(':animated')){
-						if($('#TextWrapper').css('top') == $('#TextWrapper').data("TopDefault")) {
+						if($('#TextWrapper').data("Scroll") == "Down") {
 							$('#TextWrapper').animate({
-								top: '-=70%'
+								top: '-=' + ($('#TextWrapper').position().top + $('#TextWrapper').height())  + "px"
 							}, 1000, function() {
 								$('#DownArrow').css({
 									transform: 'rotate(180deg)'
 								});
+								$('#TextWrapper').data("Scroll" ,"Up");
 							});
 							$('#TwitterWrapper').animate({
-								top: '-=70%'
+								top: '-=' + ($('#TextWrapper').position().top + $('#TextWrapper').height() + 500)  + "px"
 							}, 1000);
 						} else {
 							$('#TextWrapper').animate({
-								top: '+=70%'
+								top: '+=' + (($(window).height() / 2) + ($('#TextWrapper').height() - 120))  + "px"
 							}, 1000, function() {
 								$('#DownArrow').css({
 									transform: 'rotate(0deg)'
 								});
+								$('#TextWrapper').data("Scroll" ,"Down");
 							});
 							$('#TwitterWrapper').animate({
-								top: '+=70%'
+								top: '+=' + (($(window).height() / 2) + ($('#TextWrapper').height() - 120) + 500)  + "px"
 							}, 1000);
 						}
 					}
@@ -54,22 +56,22 @@
 	<body>
 		<div id="GlobalWrapper">
 			<div id="TextWrapper">
-				<a href="https://github.com/HeBaSoft/QuestBook" target="_blank" ><img src="Pictures/ButtonGitHub.png" id="ButtonGitHub"></a>
 				<div id="Title">QuestBook</div>
 				<div id="SubTitle">Community Update</div>
 				<div id="Text">
 					<div style="color: #BFBBAC;">QuestBook currently is unavailable due to Community update!</div><br>
 					This update will bring our community closer together!<br>
-					Keep up with this update by hashtag <a href="https://twitter.com/search?q=%23questbookcommunityupdate&src=typd">#QuestBookCommunityUpdate</a> <br>
+					Keep up with this update by hash-tag <a href="https://twitter.com/search?q=%23questbookcommunityupdate&src=typd">#QuestBookCommunityUpdate</a>.<br>
+					You can also check the code in our repository on <a href="https://github.com/HeBaSoft/QuestBook">GitHub</a>.<br>
 					Follow creator of QuestBook <a href="https://twitter.com/TheFilipsi">@TheFilipsi</a> and <a href="https://twitter.com/TheQuestBook">@TheQuestBook</a> itself on Twitter!
 				</div>
 				<div id="HiddenText">Also there's <a id="HiddenUrl" href="https://twitter.com/costiic">Vüjta</a>! Don't check him out</div>
 				<img src="Pictures/ButtonArrow.png" alt="Vujta broke dis" id="DownArrow" height="128px" width="128px">
 			</div>
 			<div id="TwitterWrapper">
-				<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=%23QuestBookCommunityUpdate%20-RT" width="400" height="450" data-chrome="nofooter noscrollbar" data-widget-id="563988252777254912">Tweets about #QuestBookCommunityUpdate -RT</a>
+				<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=%23QuestBookCommunityUpdate%20-RT" width="400" height="300" data-chrome="nofooter" data-widget-id="563988252777254912">Tweets about #QuestBookCommunityUpdate -RT</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-				<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=from%3A%40TheQuestBook%20-RT" width="400" height="450" data-chrome="nofooter noscrollbar" data-widget-id="564672975849680896">Tweets about from:@TheQuestBook -RT</a>
+				<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=from%3A%40TheQuestBook%20-RT" width="400" height="300" data-chrome="nofooter" data-widget-id="564672975849680896">Tweets about from:@TheQuestBook -RT</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			</div>
 		</div>
